de9b4db04fc285edbf70757a7590bd831c6e751b
Dhruv Parmar <dhruvparmar70441@gmail.com>
2025-01-11 10:26:56 +0530

    created useFetch hook to fetch data

--- frontend/src/BlogList.jsx
+++ frontend/src/BlogList.jsx
@@ -1,4 +1,4 @@
-const BlogList = ({blogs, title, handleDelete}) => {
+const BlogList = ({blogs, title}) => {
     return ( 
         <div>
         <h2 className='text-3xl' text>{title}</h2>
@@ -6,7 +6,6 @@ const BlogList = ({blogs, title, handleDelete}) => {
                 <div key={blog.id} className='py-2 px-4 my-6 mx-0 border-b-2 border-[#fafafa]-800 hover:shadow-md hover:shadow-[#ccc]'>
                     <h2 className='text-lg font-bold text-[#f1356d] mb-2'>{blog.title}</h2>
                     <p>written by {blog.author}</p>
-                    <button onClick={() => handleDelete(blog.id)}>Delete blog</button>
                 </div>
             ))}
         </div>
--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,22 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {title:'my new website', body:'Lorem ipsum dolor sit amet.', author:'me', id:1},
-    {title:'welcome party', body:'Lorem ipsum dolor sit amet.', author:'yoshi', id:2},
-    {title:'web dev top tips', body:'Lorem ipsum dolor sit amet.', author:'mario', id:3}
-  ])
+  
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
+  
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => (blog.id !== id));
-    setBlogs(newBlogs);
-  }
+  
   
   return (
     <div>
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title='All Blogs!' />}
     </div>
   )
 }

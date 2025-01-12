543a5e320f000cb39d4049bc1351aaa1686ec36f
Dhruv Parmar <dhruvparmar70441@gmail.com>
2025-01-13 00:07:35 +0530

    modified db connection

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ configDotenv()
 const app = express();
 
 //db connection
-mongoose.connect(`${process.env.MONGO_URI}`)
+mongoose.connect(`${process.env.MONGO_URI}/${process.env.DB_NAME}`)
 .then(()=>{
     app.listen(process.env.PORT, () => {
         console.log('connected to db');

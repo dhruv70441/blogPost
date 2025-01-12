6221b52efd3e7f88f7c794d20fc45755abc2eb48
Dhruv Parmar <dhruvparmar70441@gmail.com>
2025-01-12 22:24:50 +0530

    created blog.model.js file

---
+++ backend/models/blog.model.js
@@ -0,0 +1,21 @@
+import mongoose,{ Schema } from 'mongoose'
+
+const blogSchema = new Schema({
+    title:{
+        type: String,
+        required: true,
+    },
+    body:{
+        type: String,
+        required: true,
+    },
+    author:{
+        type: String,
+        required: true,
+    }
+},{
+    timestamps: true
+})
+
+const blogModel = mongoose.model('Blog', blogSchema);
+export default blogModel;

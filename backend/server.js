bc30bf6903c2e60336d4861987ccdd425990fd67
Dhruv Parmar <dhruvparmar70441@gmail.com>
2025-01-12 23:43:37 +0530

    change db url

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import mongoose from 'mongoose';
 configDotenv()
 
 //db connection
-mongoose.connect(`${process.env.MONGO_URI}/${process.env.DB_NAME}`)
+mongoose.connect(`${process.env.MONGO_URI}`)
 .then(()=>{
     app.listen(process.env.PORT, () => {
         console.log('connected to db');

8b16dafd10415b164721b31b105498035cfd0156
Dhruv Parmar <dhruvparmar70441@gmail.com>
2025-01-12 22:23:35 +0530

    connected db

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,29 @@
 import express from 'express';
 import { configDotenv } from 'dotenv';
 import blogRoutes from './blogs.js'
+import mongoose from 'mongoose';
 
+//dotenv configure
 configDotenv()
+
+//db connection
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    app.listen(process.env.PORT, () => {
+        console.log('connected to db');
+        console.log(`server is running on port ${process.env.PORT}`);
+    })
+    
+})
+.catch((err)=>{
+    console.log(err);
+    
+})
+
+//app intializing
 const app = express();
+
+
 
 app.use(express.json())
 
@@ -12,10 +32,5 @@ app.use((req,res,next) => {
     console.log(req.path,req.method);
     next()
 })
-
 app.use('/api/v1/blogs',blogRoutes)
 
-app.listen(process.env.PORT, () => {
-    console.log(`server is running on port ${process.env.PORT}`);
-    
-})
